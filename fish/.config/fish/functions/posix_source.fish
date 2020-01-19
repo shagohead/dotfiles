2b5eb55a2d4eec1875517da3b2998a3dc2284c75
shagohead <html.ru@gmail.com>
2020-01-19 21:10:25 +0500

    posix_source -q (quite mode)

--- fish/.config/fish/functions/posix_source.fish
+++ fish/.config/fish/functions/posix_source.fish
@@ -1,8 +1,16 @@
 function posix_source
+    set -l verbose 0
+    if test $argv[1] = '-q'
+        set -e argv[1]
+    else
+        set verbose 1
+    end
     if test -d $argv
         set argv '.env'
     end
-    echo "Setting variables from" $argv
+    if test $verbose -eq 1
+        echo "Setting variables from" $argv
+    end
     for i in (cat $argv)
         if test -z "$i"
             continue
