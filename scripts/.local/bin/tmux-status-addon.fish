fadbb2769d40da38d69146a0a76a115ccd0350ef
shagohead <html.ru@gmail.com>
2020-01-25 02:12:25 +0500

    tmux status right with artist, track from Music.app

--- scripts/.local/bin/tmux-status-addon.fish
+++ scripts/.local/bin/tmux-status-addon.fish
@@ -1,13 +1,17 @@
 #!/usr/bin/env fish
 
 set -l max_length 79
-
-# set -l playing (osascript -e 'tell application "Music" to get (artist, name) of current track' 2>/dev/null)
 set -l playing
+
+if pgrep -q Music
+    set playing (osascript -e 'tell application "Music" to get (artist, name) of current track' 2>/dev/null)
+end
+
 set -l keyboard (xkbswitch -ge)
 if test "$keyboard" = "Russian"
     set keyboard "RU"
 end
+
 set -l status_string (string join ' | ' $playing $keyboard)
 
 if test (string length $status_string) -gt $max_length
