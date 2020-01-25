a8975462f7d412d16c4c1640b86639690ddb6e35
shagohead <html.ru@gmail.com>
2020-01-26 01:51:57 +0500

    status right with battery

--- scripts/.local/bin/tmux-status-addon.fish
+++ scripts/.local/bin/tmux-status-addon.fish
@@ -1,18 +1,22 @@
 #!/usr/bin/env fish
 
 set -l max_length 79
-set -l playing
+set -l status_list
+
 
 if pgrep -q Music
-    set playing (osascript -e 'tell application "Music" to get (artist, name) of current track' 2>/dev/null)
+    set status_list (osascript -e 'tell application "Music" to get (artist, name) of current track' 2>/dev/null)
 end
+
+set status_list (pmset -g ps | rg -o '\d+%')
 
 set -l keyboard (xkbswitch -ge)
 if test "$keyboard" = "Russian"
     set keyboard "RU"
 end
+set status_list $keyboard
 
-set -l status_string (string join ' | ' $playing $keyboard)
+set -l status_string (string join ' | ' $status_list)
 
 if test (string length $status_string) -gt $max_length
     set status_string ".."(string sub -s (math "($max_length * -1) + 2") $status_string)
