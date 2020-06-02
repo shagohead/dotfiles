711dbf5181e8c71c5126e68aaac75370a9bf9ff7
shagohead <html.ru@gmail.com>
2020-06-02 19:22:19 +0500

    ctags: find all python files except jedi/ & mypy/

--- scripts/.local/bin/ctags.fish
+++ scripts/.local/bin/ctags.fish
@@ -1,25 +1,26 @@
 #!/usr/bin/env fish
 
-fd -e py
-set site_packages
+# fd -e py
+# set site_packages
 
-if test -e poetry.lock
-    set site_packages (poetry run python -c 'import sys; print(sys.path[-1])')
-end
+# if test -e poetry.lock
+#     set site_packages (poetry run python -c 'import sys; print(sys.path[-1])')
+# end
 
-if test -e Pipfile.lock
-    set python (pipenv --py 2> /dev/null)
-    if test -n $python
-        set site_packages (fish -c $python' -c "import sys; print(sys.path[-1])"')
-    end
-end
+# if test -e Pipfile.lock
+#     set python (pipenv --py 2> /dev/null)
+#     if test -n $python
+#         set site_packages (fish -c $python' -c "import sys; print(sys.path[-1])"')
+#     end
+# end
 
-if test -z "$site_packages" -a -f .venv/bin/python
-    set site_packages (fish -c '.venv/bin/python -c "import sys; print(sys.path[-1])"')
-end
+# if test -z "$site_packages" -a -f .venv/bin/python
+#     set site_packages (fish -c '.venv/bin/python -c "import sys; print(sys.path[-1])"')
+# end
 
-if test -n $site_packages
-    fd -E 'jedi/' -E 'mypy*' '.py$' $site_packages
-end
+# if test -n $site_packages
+#     fd -E 'jedi/' -E 'mypy*' '.py$' $site_packages
+# end
 
-# TODO js & node_modules
+# # TODO js & node_modules
+fd -E 'jedi/' -E 'mypy*' -e py -I -H
