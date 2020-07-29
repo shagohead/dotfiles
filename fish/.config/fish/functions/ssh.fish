ffb7fc746c4519134dc214036e10922e6314bc97
shagohead <html.ru@gmail.com>
2020-07-29 13:23:05 +0500

    ssh function with tmux title

--- fish/.config/fish/functions/ssh.fish
+++ fish/.config/fish/functions/ssh.fish
@@ -1,5 +1,19 @@
-function ssh -d "Call SSH with TERM env invoked"
-    tmux rename-window "@$argv"
+function ssh -d "Call SSH with TERM env invoked & tmux set tmux title"
+    set -l rename_window 0
+    if test "$TMUX" != ""
+        set -l automatic_rename (tmux show -v automatic-rename)
+        if test "$automatic_rename" != "off"
+            set rename_window 1
+        end
+    end
+
+    if test $rename_window -eq 1
+        tmux rename-window "@$argv"
+    end
+
     TERM=xterm-256color command ssh $argv
-    tmux set automatic-rename on
+
+    if test $rename_window -eq 1
+        tmux set automatic-rename on
+    end
 end
