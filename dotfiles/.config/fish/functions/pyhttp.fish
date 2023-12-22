2c0cb3133df2d37bd89bd68cb1ed74d0b0949a1e
shagohead <html.ru@gmail.com>
2023-12-22 13:43:22 +0300

    Remove useless func

--- dotfiles/.config/fish/functions/pyhttp.fish
+++
@@ -1,9 +0,0 @@
-function pyhttp --description "Start SimpleHTTPServer, optional argument for port number" --argument port
-    if test -z "$port"
-        set port 8888
-    end
-
-    # echo "About to serve on http://0.0.0.0:$port"
-    # # python -m SimpleHTTPServer $port;
-    # /Users/lastdanmer/Applications/http-server.py $port
-end
