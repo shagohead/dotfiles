342e187a46b0074f73a4e700dd0e475fa1773b13
shagohead <html.ru@gmail.com>
2024-01-10 20:41:59 +0300

    Prepend prompt with current time

--- dotfiles/.config/fish/functions/fish_prompt.fish
+++ dotfiles/.config/fish/functions/fish_prompt.fish
@@ -28,5 +28,5 @@ function fish_prompt --description 'Write out the prompt'
     set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
     # echo -n -s (prompt_login)' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
-    echo -n -s (set_color $color_cwd) (prompt_pwd) $normal " "$prompt_status $suffix " "
+    echo -n -s $normal (tput dim) (date '+%H:%M:%S') (tput sgr0) " " (set_color $color_cwd) (prompt_pwd) $normal " "$prompt_status $suffix " "
 end
